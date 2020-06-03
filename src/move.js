2e09c2f3d3af9684517c2d0c4d825040234062bd
Ahmad Fz <ahmad.f.zadeh@icloud.com>
2020-06-03 12:04:31 +0430

    remove method

--- src/move.js
+++ src/move.js
@@ -1,24 +1,14 @@
-import Node from './node';
-import minmax from './minmax';
-
 export default class Move {
     constructor({
         row,
         col,
-        board,
+        state,
         player
     }) {
         this.row = row;
         this.col = col;
         this.score = undefined;
-        this.board = board;
+        this.state = state;
         this.player = player;
-    }
-
-    calcScore() {
-        return minmax(new Node({
-            board: this.board,
-            player: this.player
-        }), this.player.getType(), 10);
     }
 }
