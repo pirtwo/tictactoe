bd456bf73c993f65465e0ebf4a6c59df96491bf4
Ahmad Fz <ahmad.f.zadeh@icloud.com>
2020-06-06 10:42:13 +0430

    fix some issues, add some comments

--- src/tictac-worker.js
+++ src/tictac-worker.js
@@ -1,55 +1,49 @@
 import Node from './node';
 import Move from './move';
 import Grid2D from './grid2d';
-import Player from './player';
 import minmax from './minmax';
 
 onmessage = (msg) => {
     console.log('worker started.');
-    console.log(msg.data);
+
     if (msg.data.start) {
-        let {
-            state,
-            player,
-            thoughtDepth
-        } = msg.data, moves = [];
+        const {
+            state,        // current board state
+            player,       // current player x or o
+            thoughtDepth  // the depth for minmax search
+        } = msg.data;
 
-        state.filter(cell => cell.value === 0).forEach(cell => {
-            let board = new Grid2D({
-                rowNum: 3,
-                colNum: 3,
-                cells: state.map(i => i.value)
-            });
+        const
+            moves = [],
+            currState = state.split(''),
+            grid = new Grid2D(3, 3, state.split(''));
 
-            let currPlayer = new Player({
-                sign: player.sign,
-                isCpuPlayer: player.isCpuPlayer,
-                isMaximization: player.isMaximization
-            });
-
-            let prePlayer = new Player({
-                sign: player.sign == 'x' ? 'o' : 'x',
-                isMaximization: !player.isMaximization
-            });
-
-            let node = new Node({
-                board: board,
-                player: prePlayer
-            });
-
-            let move = new Move({
-                row: cell.row,
-                col: cell.col,
-                state: state.slice(0),
-                player: currPlayer
-            });
-
-            move.score = minmax(node, currPlayer.getType(), 2);
-            moves.push(move);
-        });
+        // calc the score of each possible move for current player.
+        for (const cell of grid) {
+            if (cell.value === '0') {
+                const nextState = currState.slice(0);
+                nextState[cell.row * 3 + cell.col] = player;
+                const node = new Node(nextState.join(''), player);
+                const move = new Move({
+                    row: cell.row,
+                    col: cell.col,
+                    state: node.state,
+                    player: node.player
+                });
+                move.score = minmax(
+                    node,
+                    // we assuming the x is max and o is min,
+                    // if current player is a x, the next move will be min,
+                    // if current player is a o, the next move will be max.
+                    player.sign === 'x' ? 'min' : 'max',
+                    thoughtDepth
+                );
+                moves.push(move);
+            }
+        }
 
         let bestMove = moves.find(move => {
-            if (player.isMaximization)
+            if (player.sign === 'x')
                 return move.score === Math.max.apply({}, moves.map(i => i.score));
             else
                 return move.score === Math.min.apply({}, moves.map(i => i.score));
